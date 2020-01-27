b21a56b2824bff6f3a86061b8ac7a1f7b9b7be85
saramarin23 <saramarin.econ@gmail.com>
2020-01-27 19:50:54 +0100

    add material/ui and home page with hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 const App = ({}) => {
-  return <div>Hola</div>;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Home}></Route>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  Grid,
+  TextField,
+  Button
+} from "@material-ui/core";
 
-export default () => <div>Home</div>;
+export default () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchTextChange = event => {
+    setSearchText(event.target.value);
+  };
+
+  const handleResetText = event => {
+    console.log("Reset");
+  };
+
+  const handleSearchText = event => {
+    console.log("Search");
+  };
+
+  return (
+    <Container>
+      <Card>
+        <Grid container>
+          <Grid>
+            <Typography>Bienvenido!</Typography>
+          </Grid>
+          <Grid>
+            <label>Icono</label>
+          </Grid>
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Buscar..."
+          onChange={handleSearchTextChange}
+        />
+        <Grid>
+          <Button variant="contained" onClick={handleResetText}>
+            Reset
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSearchText}
+          >
+            Search
+          </Button>
+        </Grid>
+      </Card>
+    </Container>
+  );
+};
